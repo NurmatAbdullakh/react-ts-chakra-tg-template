89b847195a859cc105013f1a5486f39ea8344d2f
Nurmat_Abdullaev <anstronnna@gmail.com>
2024-02-27 17:47:25 +0500

    feat : add ChakraProvider and QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css'
 import useTelegram from './hooks/useTelegram'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ChakraProvider } from '@chakra-ui/react';
+
+const queryClient = new QueryClient()
 
 function App() {
   const tg = useTelegram()
@@ -17,7 +21,11 @@ function App() {
   
   
   return (
-   <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
